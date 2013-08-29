a34f12e07acdb33c658a80db540faed6f625fdf5
Joachim Wuttke (laptop) <j.wuttke@fz-juelich.de>
2013-08-29 12:56:41 +0200

    This is release lmfit-5.1. Corrected initialization of control->msgfile (was stream).

--- lib/lmstruct.h
+++ lib/lmstruct.h
@@ -53,7 +53,7 @@ typedef struct {
                          to patience*(number_of_parameters+1). */
     int scale_diag;   /* If 1, the variables will be rescaled internally.
                          Recommended value is 1. */
-    FILE** stream;    /* Pointer to output stream to write to. */
+    FILE* msgfile;    /* Progress messages will be written to this file. */
     int verbosity;    /* OR'ed: 1: print some messages; 2: print Jacobian. */
     int n_maxpri;     /* -1, or max number of parameters to print. */
     int m_maxpri;     /* -1, or max number of residuals to print. */
