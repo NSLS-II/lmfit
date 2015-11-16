6e83a2ccd73b3ab9d18ed32471127c6971282287
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2015-11-16 14:18:00 +0100

    python script to compare with test_qr

--- test/test_qr.c
+++ test/test_qr.c
@@ -10,24 +10,57 @@
  *           the manual page source lmcurve.pod and to the wiki.
  *
  * Author:   Joachim Wuttke <j.wuttke@fz-juelich.de> 2004-2013
- * 
+ *
  * Licence:  see ../COPYING (FreeBSD)
- * 
+ *
  * Homepage: apps.jcns.fz-juelich.de/lmfit
  */
- 
+
 #include "lmcurve.h"
 #include <stdio.h>
-
-/* model function: a parabola */
-
-double f( double t, const double *p )
-{
-    return p[0] + p[1]*t + p[2]*t*t;
-}
+#include <stdlib.h>
 
 void lm_qrfac(int m, int n, double *a, int *ipvt,
               double *rdiag, double *acnorm, double *wa);
+
+void set_orthogonal( int n, double *Q, double* v )
+{
+    int i, j;
+    double temp = 0;
+    for (i=0; i<n; ++i)
+        temp += v[i]*v[i];
+    for (i=0; i<n; ++i)
+        for (j=0; j<n; ++j)
+            Q[j*n+i] = ( i==j ? 1 : 0 ) - 2*v[i]*v[j]/temp;
+}
+
+void matrix_multiplication( int n, double *A, double *Q, double *R )
+{
+    int i,j,k;
+    double temp;
+    for (i=0; i<n; ++i) {
+        for (j=0; j<n; ++j) {
+            temp = 0;
+            for (k=0; k<n; ++k) {
+                temp += Q[k*n+i]*R[j*n+k];
+            }
+            A[j*n+i] = temp;
+        }
+    }
+}
+
+void matrix_transposition( int n, double *A )
+{
+    int i,j;
+    double temp;
+    for (i=0; i<n; ++i) {
+        for (j=i+1; j<n; ++j) {
+            temp = A[j*n+i];
+            A[j*n+i] = A[i*n+j];
+            A[i*n+j] = temp;
+        }
+    }
+}
 
 void print_matrix(int m, int n, double *a)
 {
@@ -40,25 +73,29 @@ void print_matrix(int m, int n, double *a)
     }
 }
 
-int main()
+int main( int argc, char *argv[] )
 {
-    double A[9] = {     0.0344,    0.4387,    0.3816,
-                        0.7655,    0.7952,    0.1869,
-                        0.4898,    0.4456,    0.6463 };
-    int ipvt[3];
-    double rdiag[3], acnorm[3], wa[3];
+    double A[9], rdiag[3], acnorm[3], wa[3];
+    int i, ipvt[3];
+
+    if ( argc!= 10 ) {
+        fprintf( stderr, "bad # args\n" );
+        exit(1);
+    }
+    for ( int i=0; i<9; ++i )
+        A[i] = atof( argv[1+i] );
 
     printf( "Input matrix A:\n" );
     print_matrix(3, 3, A);
-    
+
     lm_qrfac(3, 3, A, ipvt, rdiag, acnorm, wa);
 
     printf( "Ouput matrix A:\n" );
     print_matrix(3, 3, A);
-    
+
     printf( "Ouput vector rdiag:\n" );
     print_matrix(1, 3, rdiag);
-    
+
     printf( "Ouput vector acnorm:\n" );
     print_matrix(1, 3, acnorm);
 }
