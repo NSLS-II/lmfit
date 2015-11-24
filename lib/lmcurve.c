bbc3fe5cc3075f90f6e195e4f60dd8f8e99d3963
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2015-11-24 16:31:34 +0100

    autoformatted other source files; Use temporary variable to improve readability in lmcurve_evaluate (patch by tklauser)

--- lib/lmcurve.c
+++ lib/lmcurve.c
@@ -15,37 +15,30 @@
 
 #include "lmmin.h"
 
-
 typedef struct {
-    const double *t;
-    const double *y;
-    double (*f) (double t, const double *par);
+    const double* t;
+    const double* y;
+    double (*f)(double t, const double* par);
 } lmcurve_data_struct;
 
-
-void lmcurve_evaluate( const double *par, int m_dat, const void *data,
-                       double *fvec, int *info )
+void lmcurve_evaluate(const double* par, int m_dat, const void* data,
+                      double* fvec, int* info)
 {
+    lmcurve_data_struct* D = (lmcurve_data_struct*)data;
     int i;
-    for ( i = 0; i < m_dat; i++ )
-        fvec[i] =
-            ((lmcurve_data_struct*)data)->y[i] -
-            ((lmcurve_data_struct*)data)->f(
-                ((lmcurve_data_struct*)data)->t[i], par );
+    for (i = 0; i < m_dat; i++)
+        fvec[i] = D->y[i] - D->f(D->t[i], par);
 }
 
-
-void lmcurve( int n_par, double *par, int m_dat,
-              const double *t, const double *y,
-              double (*f)( double t, const double *par ),
-              const lm_control_struct *control,
-              lm_status_struct *status )
+void lmcurve(int n_par, double* par, int m_dat, const double* t,
+             const double* y, double (*f)(double t, const double* par),
+             const lm_control_struct* control, lm_status_struct* status)
 {
     lmcurve_data_struct data;
     data.t = t;
     data.y = y;
     data.f = f;
 
-    lmmin( n_par, par, m_dat, (const void*) &data,
-           lmcurve_evaluate, control, status );
+    lmmin(n_par, par, m_dat, (const void*)&data, lmcurve_evaluate, control,
+          status);
 }
